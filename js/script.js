5fa661adb622136643a6152fee6aef64e984bf01
Büşra Özata <busraozata1998@gmail.com>
2023-02-07 18:31:32 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,59 @@
+const menuToggle = document.querySelector("#toggle-btn");
+const navigation = document.querySelector(".navigation");
+menuToggle.onclick = function () {
+  menuToggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+
+  if (menuToggle.innerHTML === `<i class="fa-solid fa-xmark"></i>`) {
+    menuToggle.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+  } else {
+    menuToggle.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+  }
+};
+
+$("ul li").on("click", function () {
+  $(this).toggleClass("show");
+  $(this).siblings().removeClass("show");
+});
+
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+});
+
 var swiper = new Swiper(".mySwiperBanner", {
   slidesPerView: 1,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 var swiper = new Swiper(".mySwiperBlog", {
-  slidesPerView: 2,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
   },
 });
 var mySwiper = new Swiper(".swiper-container", {
@@ -17,24 +61,49 @@ var mySwiper = new Swiper(".swiper-container", {
   observeParents: true,
   //autoplay: true,
   speed: 200,
-  loop: true,
-  slidesPerView: 5,
-  spaceBetween: 15,
-  slidesPerGroup: 1,
+
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
   breakpoints: {
+    720: {
+      slidesPerView: 2,
+      spaceBetween: 5,
+      slidesPerGroup: 1,
+    },
     960: {
-      slidesPerView: 3,
+      slidesPerView: 1,
       spaceBetween: 10,
       slidesPerGroup: 1,
     },
-    720: {
+
+    1024: {
       slidesPerView: 3,
-      spaceBetween: 5,
+      spaceBetween: 85,
       slidesPerGroup: 1,
+    },
+  },
+});
+
+var swiper = new Swiper(".mySwiperImageGallery", {
+  spaceBetween: 30,
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
     },
   },
 });
