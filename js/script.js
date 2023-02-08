09d9867759fe358587497197308760a3a2b1a3ff
Büşra Özata <busraozata1998@gmail.com>
2023-02-08 18:26:34 +0300

    admin change css

--- js/script.js
+++ js/script.js
@@ -73,12 +73,12 @@ var mySwiper = new Swiper(".swiper-container", {
   breakpoints: {
     720: {
       slidesPerView: 2,
-      spaceBetween: 5,
+      spaceBetween: 50,
       slidesPerGroup: 1,
     },
     960: {
       slidesPerView: 1,
-      spaceBetween: 10,
+      spaceBetween: 30,
       slidesPerGroup: 1,
     },
 
@@ -106,4 +106,40 @@ var swiper = new Swiper(".mySwiperImageGallery", {
       spaceBetween: 30,
     },
   },
+});
+
+const slider = document.querySelector(".slider");
+const sl = new Swiper(slider, {
+  slidesPerView: "auto",
+  loop: true,
+  speed: 3000,
+  slidesPerView: "8",
+  autoplay: {
+    enabled: true,
+    delay: 1,
+  },
+});
+
+
+$(function () {
+  $('a[href="#search"]').on("click", function (event) {
+    event.preventDefault();
+    $("#search").addClass("open");
+    $('#search > form > input[type="search"]').focus();
+  });
+
+  $("#search, #search button.close").on("click keyup", function (event) {
+    if (
+      event.target == this ||
+      event.target.className == "close" ||
+      event.keyCode == 27
+    ) {
+      $(this).removeClass("open");
+    }
+  });
+
+  $("form").submit(function (event) {
+    event.preventDefault();
+    return false;
+  });
 });
